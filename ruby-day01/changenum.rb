cf344bca84b16d52abb0390669f1a773efc2a110
alex <alex830216@gmail.com>
2021-08-02 22:56:37 +0800

    x&y交換值

---
+++ ruby-day01/changenum.rb
@@ -0,0 +1,29 @@
+x = "11"
+y = [1]
+
+x, y = y, x
+
+p x, y
+
+
+
+# 換數字
+
+# x = 2
+# y = 3
+
+# tmp = x
+# x = y
+# y = tmp
+
+# puts x
+# puts y
+
+# x = 2
+# y = 3
+
+# Ruby的特殊寫法
+# x, y = y, x
+
+# puts x
+# puts y
